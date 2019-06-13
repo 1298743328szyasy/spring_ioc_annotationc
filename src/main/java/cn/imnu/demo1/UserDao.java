9cadf289060ef2df7c6995da518a31c23e0b5a25
1298743328szyasy <1298743328@qq.com>
2019-06-13 20:43:30 +0800

    新增标签

---
+++ src/main/java/cn/imnu/demo1/UserDao.java
@@ -0,0 +1,10 @@
+package cn.imnu.demo1;
+
+import org.springframework.stereotype.Repository;
+
+@Repository("userDao")
+public class UserDao {
+    public void save(){
+        System.out.println("Dao中保存用户");
+    }
+}
