dd8f966027d4c9376e166f2115693e9d8e52dfbb
1298743328szyasy <1298743328@qq.com>
2019-06-13 20:56:18 +0800

    新注解

--- src/main/java/cn/imnu/demo1/UserService.java
+++ src/main/java/cn/imnu/demo1/UserService.java
@@ -2,18 +2,20 @@ package cn.imnu.demo1;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-/**
- *
- */
+import javax.annotation.Resource;
+
 //@Component("userService")
     @Service("userService")
 public class UserService {
     @Value("米饭")
     private String something;
-    @Autowired
+//    @Autowired
+//    @Qualifier("userDao")
+    @Resource(name = "userDao")
     private UserDao userDao;
     public String sayHello(String name) {
         return "Hello" + name;
