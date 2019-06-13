caa2ed4a0360c81c93dfde61f08018f434edaa72
1298743328szyasy <1298743328@qq.com>
2019-06-13 21:15:50 +0800

    同上

--- src/main/java/cn/imnu/demo2/Bean2.java
+++ src/main/java/cn/imnu/demo2/Bean2.java
@@ -1,8 +1,10 @@
 package cn.imnu.demo2;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("bean2")
+@Scope("prototype")
 public class Bean2 {
 
 }
