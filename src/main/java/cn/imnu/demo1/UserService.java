c7e6b558a42d07d6126382d6e6e50cb10fa1de98
1298743328szyasy <1298743328@qq.com>
2019-06-13 20:32:46 +0800

    1

--- src/main/java/cn/imnu/demo1/UserService.java
+++ src/main/java/cn/imnu/demo1/UserService.java
@@ -1,6 +1,7 @@
 package cn.imnu.demo1;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +13,16 @@ import org.springframework.stereotype.Service;
 public class UserService {
     @Value("米饭")
     private String something;
-
+    @Autowired
+    private UserDao userDao;
     public String sayHello(String name) {
         return "Hello" + name;
     }
     public void eat(){
         System.out.println("eat:"+something);
     }
-
+    public void save(){
+        System.out.println("Service中保存用户");
+        userDao.save();
+    }
 }
