371460e6b618b1c3b84931131c1079ff3a763041
1298743328szyasy <1298743328@qq.com>
2019-06-13 20:32:38 +0800

    1

--- src/main/java/cn/imnu/demo1/SpringDemo1.java
+++ src/main/java/cn/imnu/demo1/SpringDemo1.java
@@ -14,6 +14,14 @@ public class SpringDemo1 {
     }
     @Test
     public void demo2(){
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("")
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserService userService = (UserService) applicationContext.getBean("userService");
+        userService.eat();
+    }
+    @Test
+    public void demo3(){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserService userService = (UserService) applicationContext.getBean("userService");
+        userService.save();
     }
 }
